ec96ad4c3195742ed83028e6f329dee52dda8c64
Subham Mani <manisubham09@gmail.com>
2023-11-13 13:37:58 +0530

    learn parcel, package.json, install react via npm

---
+++ ep02/igniting-app/app.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+const parent = React.createElement("div", { id: "parent" }, 
+                [
+                    React.createElement("div", { id: "child1" }, [
+                        React.createElement("h1", {}, "I'm h1 Tag"), React.createElement("h2", {}, "I'm h2 Tag")
+                    ]
+                    ),
+                    React.createElement("div", { id: "child2" }, [
+                        React.createElement("h1", {}, "I'm h1 Tag"), React.createElement("h2", {}, "I'm h2 Tag")
+                    ]
+                    )
+                ]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);

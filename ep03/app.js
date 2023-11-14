6d34b7fc22f2bf1fe252711ae84a85f7c53abcca
Subham Mani <manisubham09@gmail.com>
2023-11-15 01:06:21 +0530

    upload coding assignments

--- ep03/app.js
+++ ep03/app.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import logo from "./src/logo.svg";
+import user_logo from "./src/github.png";
 
+/*
 // JSX => React.createElement => React Element (JS Object) => HTML Element
 
 // React Element
@@ -27,7 +30,33 @@ const HeadingComponent = () => (
     {TitleComponent()}
     <h1>Namaste React by React Component</h1>
   </div>
+);*/
+
+// Assignment 1
+/*
+const TitleComponent = () => <h1>Namaste</h1>;
+
+const HeaderComponent = () => (
+  <div className="title">
+    <TitleComponent />
+    <h2>World</h2>
+    <h3>!!</h3>
+  </div>
 );
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent />);
+root.render(<HeaderComponent />);*/
+
+// Assignment 2
+
+const HeaderComponent = () => (
+  <div className="header">
+    <img src={logo} className="header-logo" />
+    <input type="search" />
+    <img src={user_logo} className="user-logo" />
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeaderComponent />);
